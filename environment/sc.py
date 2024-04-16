9739d0d80d3448dc6ded3df71d59d787b252d16a
Smashliker <bengame@hotmail.no>
2024-04-16 23:52:31 +0200

    Added virtual link BW requirements to the SCs

--- environment/sc.py
+++ environment/sc.py
@@ -6,10 +6,15 @@ class ServiceChain:
         arrivalTime: float,
         ttl: float,
         vnfs: List[dict[str, float]],
-        max_response_latency: float
+        max_response_latency: float,
+        virtualLinkRequirements: List[float]
     ):
         self.arrivalTime = arrivalTime
         self.ttl = ttl
         self.vnfs = vnfs
         self.maxCumulativeLatency = max_response_latency
         self.num_vnfs = len(self.vnfs)
+
+        assert len(virtualLinkRequirements) == len(vnfs)
+        self.virtualLinkRequirements = virtualLinkRequirements
+        self.virtualLinkRequirements[0] = -1 #Add a dummy value to the first one, since the first VNF does not require a link
