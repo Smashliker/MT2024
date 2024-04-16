c10eb17c4701f349f84c0d99048d713012cc3a9e
Smashliker <bengame@hotmail.no>
2024-04-16 23:51:03 +0200

    Updated graph_generator to fit requirements for enhanced toy, and to fit with FL

--- graph_generator.py
+++ graph_generator.py
@@ -8,8 +8,8 @@ import sys
 from typing import List
 
 DEFAULTOUTPUTFILE = r"./data/network"
-DEFAULTGRAPHTYPE = "zhang"
-DEFAULTSEED = None
+DEFAULTGRAPHTYPE = "enhancedFederated"
+DEFAULTSEED = 0
 
 def saveGraph(
         graph: nx.Graph,
@@ -31,11 +31,14 @@ def saveGraph(
         print(graph.nodes(data=True))
         print()
         print("edges:")
-        print(nx.to_dict_of_dicts(graph))
+        print(nx.to_dict_of_dicts(graph)) 
+        print()
+        print("overview:")
+        print(graph)
 
         sys.stdout = oldSys
 
-def generateToyGraph() -> nx.Graph:
+def generateToyGraph() -> List[nx.Graph]:
     """
     Function that creates a graph that corresponds to the specifications of the toy implementation.
     """
@@ -46,22 +49,24 @@ def generateToyGraph() -> nx.Graph:
     for (nodeIndex, theCPU) in enumerate(CPUs):
         returnGraph.add_node(nodeIndex, cpu=theCPU)
 
-    return returnGraph
+    return [returnGraph]
 
 def generateZhangGraph(
         seed: int = DEFAULTSEED
-        ) -> nx.Graph:
+        ) -> List[nx.Graph]:
     """
     Function that emulates the network found in https://ieeexplore.ieee.org/document/9749937 by Zhang
     """
+    if seed != None:
+        random.seed(seed)
     
     returnGraph = nx.Graph()
     nNodes = 20
 
-    nodeCPUs = generateNUniformValues(nNodes, [10, 50], seed)
+    nodeCPUs = generateNUniformValues(nNodes, [10, 50])
     nodeCPUs = np.rint(nodeCPUs).tolist()
 
-    nodeLatencies = generateNUniformValues(nNodes, [10, 40], seed)
+    nodeLatencies = generateNUniformValues(nNodes, [10, 40])
 
     for nodeIndex in range(nNodes):
         returnGraph.add_node(nodeIndex, cpu=nodeCPUs[nodeIndex], latency=nodeLatencies[nodeIndex])
@@ -93,26 +98,162 @@ def generateZhangGraph(
     ]
     nLinks = len(links)
 
-    linkBWs = generateNUniformValues(nLinks, [15, 30], seed)
-    linkLatencies = generateNUniformValues(nLinks, [15, 30], seed)
+    linkBWs = generateNUniformValues(nLinks, [15, 30])
+    linkLatencies = generateNUniformValues(nLinks, [15, 30])
 
     for linkIndex, linkTuple in enumerate(links):
         returnGraph.add_edge(linkTuple[0], linkTuple[1], bandwidth=linkBWs[linkIndex], latency=linkLatencies[linkIndex])
 
-    return returnGraph
+    return [returnGraph]
 
+def generateEnhancedGraph(
+        seed: int = DEFAULTSEED,
+        singleNetwork: bool = False
+        ) -> List[nx.Graph]:
+    
+    if seed != None:
+        random.seed(seed)
+
+    returnGraph = nx.Graph()
+
+    spaceIndices = range(0, 10)
+    airIndices = range(10, 40)
+    groundIndices = range(40, 100)
+
+    totalNodes = len(spaceIndices) + len(airIndices) + len(groundIndices)
+
+    threeDomains = [spaceIndices, airIndices, groundIndices]
+    threeCPURanges = [[20, 40], [20, 40], [50, 100]]
+    threeStorageRanges = [[50, 80], [50, 80], [50, 80]]
+
+    #Creation of nodes
+    for domainIndex, domain in enumerate(threeDomains):
+        domainCPURange = threeCPURanges[domainIndex]
+        domainStorageRange = threeStorageRanges[domainIndex]
+
+        for nodeIndex in domain:
+            addSingleNode(returnGraph, nodeIndex, domainCPURange, [10, 40], domainStorageRange)
+
+    sumOfEdges = 600
+
+    domainEdges = [45, sumOfEdges * len(airIndices) / totalNodes, sumOfEdges * len(groundIndices) / totalNodes]
+    domainEdges = np.cumsum(domainEdges).tolist()
+
+    domainBWRanges = [[50, 100], [60, 100], [50, 100]]
+
+    #TODO: Consider BFS like Fredrik did
+    #This creates a direct line of links for each node: 0<->1<->2... as a baseline so that all nodes have at least one connection
+    for sourceNode in range(totalNodes - 1):
+        destinationNode = sourceNode + 1
+
+        domainTuple = (mapNodeToDomain(sourceNode, threeDomains), mapNodeToDomain(destinationNode, threeDomains))
+
+        if domainTuple[0] == domainTuple[1]:
+            linkBW = generateNUniformValues(1, domainBWRanges[domainTuple[0]])[0]
+            linkLatency = generateNUniformValues(1, [15, 30])[0]
+            
+            returnGraph.add_edge(sourceNode, destinationNode, bandwidth=linkBW, latency=linkLatency)
+
+    #Creation of links
+    while returnGraph.number_of_edges() < sumOfEdges:
+        node = random.randint(0, totalNodes - 1)
+
+        #This way of determining the relevant domain makes the links uniformly distributed (not taking the link-saturation of the space domain into account)
+        domainIndex = mapNodeToDomain(node, threeDomains)
+
+        nodes = threeDomains[domainIndex]
+        
+        linkNodes = random.sample(nodes, 2)
+
+        if returnGraph.number_of_edges(linkNodes[0], linkNodes[1]) < 1:
+            linkBW = generateNUniformValues(1, domainBWRanges[domainIndex])[0]
+            linkLatency = generateNUniformValues(1, [15, 30])[0]
+
+            returnGraph.add_edge(linkNodes[0], linkNodes[1], bandwidth=linkBW, latency=linkLatency)
+
+    interDomainBW = generateNUniformValues(2, [50, 100])
+    interDomainLatency = generateNUniformValues(2, [30, 60])
+
+    returnGraphs = []
+    if not singleNetwork:
+        padding = totalNodes
+
+        for _, domain in enumerate(threeDomains):
+            toAppend = returnGraph.subgraph(domain).copy() #NOTE: The importance of creating a mutable copy of the immuatable subgraph
+
+            padGraphToN(toAppend, padding)
+
+            returnGraphs.append(toAppend) 
+
+
+
+    #Creation of inter-domain links must happen after division of network into subgraphs
+    for interDomainIndex in range(len(threeDomains) - 1):
+        secondDomainIndex = interDomainIndex + 1
+
+        domainNodeTuple = (random.sample(threeDomains[interDomainIndex], 1)[0], random.sample(threeDomains[secondDomainIndex], 1)[0])
+
+        returnGraph.add_edge(domainNodeTuple[0], domainNodeTuple[1], bandwidth=interDomainBW[interDomainIndex], latency=interDomainLatency[interDomainIndex])
+
+    
+
+    returnGraphs.insert(0, returnGraph)
+    return returnGraphs
+
+
+def addSingleNode(
+        graph: nx.Graph,
+        nodeIndex: int,
+        CPURange: List[int],
+        latencyRange: List[int],
+        storageRange: List[int]
+        ) -> None:
+    """
+    The preferred function for adding a node to the graph. This collects all logic for creating nodes in a single function.
+    """
+    
+    nodeCPUs = generateNUniformValues(1, CPURange)
+    nodeCPU = np.rint(nodeCPUs).tolist()[0]
+
+    nodeStorage = generateNUniformValues(1, storageRange)[0]
+
+    nodeLatency = generateNUniformValues(1, latencyRange)[0]
+
+    graph.add_node(nodeIndex, cpu=nodeCPU, storage=nodeStorage, latency=nodeLatency)
+
+
+def padGraphToN(
+        graph: nx.Graph,
+        n: int
+        ) -> None:
+    zeroInterval = [0, 0]
+
+    potentialNodeIndex = 0
+    while graph.number_of_nodes() < n:
+        if potentialNodeIndex not in graph:
+            addSingleNode(graph, potentialNodeIndex, zeroInterval, zeroInterval, zeroInterval) #Pad graph with dummy nodes without resources nor links
+
+        potentialNodeIndex += 1
+
+def mapNodeToDomain(
+        node: int,
+        domains: List[List[int]] = [range(0, 10), range(10, 40), range(40, 100)]
+        ) -> int:
+    
+    for domainIndex, domain in enumerate(domains):
+        if node in domain:
+            return domainIndex
+        
+    raise Exception(f"Supplied node '{node}' not in nested list of domains.")
+    
 
 def generateNUniformValues(
         n: int = 10,
-        distributionEndPoints: List[int] = [50, 100],
-        seed: int = DEFAULTSEED   
+        distributionEndPoints: List[int] = [50, 100] 
         ) -> List[float]:
     """
     Function that creates a list of length n of randomly distributed values
     """
-
-    if seed != None:
-        random.seed(seed)
 
     values = []
 
@@ -135,7 +276,7 @@ def makeFullMesh(graph: nx.Graph) -> None:
         
         if firstNeighborIndex < graphSize:
             for secondIndex in range(firstNeighborIndex, graphSize):
-                graph.add_edge(mainIndex, secondIndex, latency=50.0) #NOTE: Latency placeholder for now
+                graph.add_edge(mainIndex, secondIndex, latency=50.0)
 
 
 if __name__ == "__main__":
@@ -158,13 +299,24 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.graphType == "toy":
-        graph = generateToyGraph()
+        graphs = generateToyGraph()
     elif args.graphType == "zhang":
-        graph = generateZhangGraph(args.seed)
+        graphs = generateZhangGraph(args.seed)
+    elif args.graphType == "enhanced":
+        graphs = generateEnhancedGraph(args.seed, singleNetwork=True)
+    elif args.graphType == "enhancedFederated":
+        graphs = generateEnhancedGraph(args.seed)
     elif args.graphType == "proper":
         sys.exit()
     else:
         raise ValueError("Not valid graphtype!")
 
+    if len(graphs) == 1:
+        names = [""]
+    elif len(graphs) == 4:
+        names = ["", "_space", "_air", "_ground"]
+    else:
+        raise ValueError("Invalid amount of graphs!")
 
-    saveGraph(graph, args.outputfile)
+    for graphIndex, graph in enumerate(graphs):
+        saveGraph(graph, f"{args.outputfile}{names[graphIndex]}")
